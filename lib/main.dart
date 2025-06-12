022108ddce38bee4c0daafa7ba69d04e572c0aca
misadowsk <michalsad.protondynamic@gmail.com>
2025-06-12 08:27:03 +0200

    feat: added firebase analytics and log that app started

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'firebase_options.dart';
 import 'app.dart';
+
+FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +14,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  analytics.logEvent(name: 'app_start', parameters: null);
 
   runZonedGuarded(
     () => runApp(const MyApp()),
